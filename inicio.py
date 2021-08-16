a6553e4ab622c5411cbd8bcbf8f7f90e95993830
natha <nathanaelmartinezcisneros@gmail.com>
2021-08-16 18:58:24 -0500

    comit open img

--- inicio.py
+++ inicio.py
@@ -1,6 +1,7 @@
 from cv2 import cv2 
 imagen = cv2.imread('contorno.jpg')
 
+print(cv2.__version__)
 cv2.imshow('imagen',imagen)
 cv2.waitKey(0) #val 0 para videso y val 1 para fotos
-cv2.destroyAllWindowa()
+cv2.destroyAllWindows()
