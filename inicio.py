0026be5ae319f83b5328f1df9fb2372da0fbc71b
natha <nathanaelmartinezcisneros@gmail.com>
2021-08-16 18:48:12 -0500

    abrir img con open cv

--- inicio.py
+++ inicio.py
@@ -1,3 +1,6 @@
-#inicia el proyecto 
-print('inicio')
-print('inicio2')
+from cv2 import cv2 
+imagen = cv2.imread('contorno.jpg')
+
+cv2.imshow('imagen',imagen)
+cv2.waitKey(0) #val 0 para videso y val 1 para fotos
+cv2.destroyAllWindowa()
